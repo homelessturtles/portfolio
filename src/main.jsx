89506dc09df3598ad2e891b665197089e98b0738
homelessturtles <86428780+homelessturtles@users.noreply.github.com>
2024-09-24 21:43:57 -0700

    dark theme init

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
+import { ColorModeScript } from "@chakra-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App.jsx";
+import theme from "./theme";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </StrictMode>
